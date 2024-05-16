0db7fe0ef67c1533dc5150f15c1c172ece421166
Nischal Gupta <nischal@onecorp.co.in>
2024-05-16 14:45:37 +0530

    Natal scetion beautification

--- pages/kona-lords.js
+++ pages/kona-lords.js
@@ -1,116 +1,117 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react"
-import astroServer from "../constants/url"
-import { KonaCard } from "../components/konaCard";
-import CommanLayout from "../layouts/comman";
-import { KonaLordCard } from "../components/konaLordCard";
-import { useQuery, useQueryClient, QueryClient, QueryClientProvider } from "react-query";
-import axios from "axios";
-
-
-
+import React, { useState } from 'react';
+import CommanLayout from '../layouts/comman';
 
 const KonaLord = () => {
-    const konaInfo = [{
-        kona: 'dharma',
-        color: 'red'
-    },
-    {
-        kona: 'artha',
-        color: 'brown'
-    },
-    {
-        kona: 'kama',
-        color: 'aqua'
-    },
-    {
-        kona: 'mokasha',
-        color: 'blue'
-    }
-    ]
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    const router = useRouter()
-    let kona = router.query.kona
-
-    const konaObj = {
-        Fire: [0, 4, 8],
-        Earth: [1, 5, 9],
-        Air: [2, 6, 10],
-        Water: [3, 7, 11],
-    }
-    console.log('konaObj ---', konaObj[kona]);
-
-    //============================================================
-    const [planet, setPlanet] = useState([])
-    const [house, setHouse] = useState([])
-    const [currentTransit, setCurrentTransit] = useState({})
-    //============================================================
-    const fetchData = async () => {
-        // const response = await astroServer.get('/user/get');
-        const response = await astroServer.get("/user/get");
-
-        if (!response) {
-            throw new Error('Network response was not ok');
-        }
-        return response;
+    const handleToggle = (index) => {
+        console.log(index);
+        setActiveIndex(activeIndex === index ? null : index);
     };
 
-    const { status, data, error, isFetching } = useQuery('postData', fetchData);
-
-    console.log('react query data ----', data?.data[0]?.houses);
-    // console.log(' data ----', response);
-    // useEffect(() => {
-    //     astroServer.get('/user/get').then(res => {
-    //         setPlanet(res?.data[0]?.planets[0]);
-    //         setHouse(res?.data[0]?.houses);
-    //     })
-    // }, [])
-
-    let konaLordData = []
-    if (house) {
-        konaLordData = [
-            { KonaHouseDetails: data?.data[0]?.houses[konaObj[kona][0]], },
-            { KonaHouseDetails: data?.data[0]?.houses[konaObj[kona][1]] },
-            { KonaHouseDetails: data?.data[0]?.houses[konaObj[kona][2]] },
-
-        ]
-    }
-    else {
-        konaLordData = []
-    }
-
-    // console.log('----- house xxx-----', konaLordData);
-    // console.log('----- house 1-----', house[konaObj[kona][0]]);
-    // console.log('----- house 2-----', house[konaObj[kona][1]]);
-    // console.log('----- konaLordData-----', konaLordData);
-
     return (
-        <>
+        <CommanLayout>
+            <div className="bg-gray-900 text-white min-h-screen p-6 rounded-xl">
+                <ul className="bg-custom-gradient flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 mb-5 rounded-xl">
+                    <li className="me-2">
+                        <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Value</a>
+                    </li>
+                    <li className="me-2">
+                        <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Value</a>
+                    </li>
+                    <li className="me-2">
+                        <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Skill</a>
+                    </li>
+                    <li className="me-2">
+                        <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Root</a>
+                    </li>
+                </ul>
 
-
-            <CommanLayout>
-
-
-                <div className="kona-card-container">
-                    {konaLordData.map(x => {
-                        console.log(x?.KonaHouseDetails);
-                        return (
-                            <KonaLordCard
-                                house={x?.KonaHouseDetails?.bhava}
-                                method={() => { router.push(`/detail/?house-owner=${x.KonaHouseDetails.owner}&rashi=${x.KonaHouseDetails.rashi}`) }}
-                                rashi={x?.KonaHouseDetails?.rashi}
-                                owner={x?.KonaHouseDetails?.owner}
-                                key={x?.KonaHouseDetails?._id}
-                            />
-                        )
-                    }
-                    )}
-
-
+                <div className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4 text-center">Planets sitting in the VI house</h2>
+                    <div className="flex flex-wrap justify-center space-x-4">
+                        {["6th & 9th Lord", "6th & 9th Lord", "6th & 9th Lord"].map((text, index) => (
+                            <div key={index} className="bg-custom-gradient p-4 text-center rounded-xl mt-3">
+                                <div className="bg-[#242538] p-4 shadow-slate-500 flex items-center justify-center rounded-xl">
+                                    <div>
+                                        <img
+                                            src={`./planets/sun.svg`}
+                                            alt="Natal"
+                                            width={45}
+                                        />
+                                    </div>
+                                </div>
+                                <p className="mt-2 font-bold text-sm">{text}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </CommanLayout>
-        </>
-    )
-}
 
-export default KonaLord
+                <div className="mb-8">
+                    <h2 className="text-xl font-semibold mb-4 text-center">Planets through the zodiac</h2>
+                    <div className="bg-[#3D3E4F] p-4 rounded-xl">
+                        {["Saturn", "Moon", "Saturn"].map((planet, index) => (
+                            <div key={index}>
+                                <div className="text-center mb-4 ">
+                                    <div
+                                        className="space-x-2 mb-2 mt-3 flex items-between justify-between cursor-pointer"
+                                        onClick={() => handleToggle(index)}
+                                    >
+                                        <div className="flex items-center">
+                                            <div className={`bg-${planet === "Saturn" ? "red" : "green"}-500 rounded-full w-2 h-2 mr-2`} />
+                                            <p className="text-sm">{planet}</p>
+                                        </div>
+                                        <p className="text-sm font-bold">20th May, 2024</p>
+                                    </div>
+                                    <div className={`accordion-content ${activeIndex === index ? "block" : "hidden"} ease-out text-center bg-[#A1A1B0] p-4 rounded-xl`}>
+                                        <h3 className="font-extrabold mb-2 text-white">{planet} in Shatabhisha:</h3>
+                                        <p className="text-sm text-white">
+                                            Independence, innovation, analytical prowess, visionary thinking. Unique values, innovative financial strategies, strategic communication.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="mb-8 ">
+                    <h2 className="text-xl font-semibold mb-4 text-center">Upcoming Saturn Event</h2>
+                    <div className="bg-custom-gradient p-4 rounded-xl flex">
+                        <div className="flex items-center">
+                            <img
+                                src={`./planets/sun.svg`}
+                                alt="Natal"
+                                width={45}
+                                class='mr-5'
+                            />
+                        </div>
+                        <div>
+
+                            <div className='text-[#767682] text-sm font-extrabold'>Sagittarius Event</div>
+                            <div>Sun enters in Leo</div>
+                            <div className="text-gray-500 text-sm">
+                                <div className='bg-green-500 rounded-full w-2 h-2 mr-2' />
+                                Event does happened</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    <h2 className="text-xl font-semibold mb-4">People going with same transit house effect</h2>
+                    <div className="flex justify-center space-x-4">
+                        {["Prachi", "Prachi"].map((name, index) => (
+                            <div key={index} className="bg-gray-800 p-4 rounded text-center">
+                                <div className="h-16 w-16 bg-gray-500 rounded-full mx-auto mb-2"></div>
+                                <div>{name}</div>
+                                <div>DOB: 14th Jul, 1994</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </CommanLayout>
+    );
+};
+
+export default KonaLord;
