b8c23644c0cbdafb485119a24e3815bdad0afe70
Nischal Gupta <nischal@onecorp.co.in>
2024-07-08 18:58:58 +0530

    planetary events

--- constants/urlPython.js
+++ constants/urlPython.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 
 const ephemerisServer = axios.create({
-  // baseURL: "https://ephemeris.onrender.com",
+  baseURL: "https://ephemeris.onrender.com",
   // baseURL: "http://192.168.29.194:8000",
-  baseURL: "http://192.168.0.115:8000",
+  // baseURL: "http://192.168.0.115:8000",
 });
 
 
