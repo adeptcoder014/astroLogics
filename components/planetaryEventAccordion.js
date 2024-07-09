ab068f80f9d83d5ab5530d09bb57d64d022242c6
Nischal Gupta <nischal@onecorp.co.in>
2024-07-09 19:01:15 +0530

    commt

--- components/planetaryEventAccordion.js
+++ components/planetaryEventAccordion.js
@@ -9,31 +9,24 @@ export const PlanetaryEventAccordion = ({ planetEvent }) => {
         setActiveIndex(activeIndex === index ? null : index);
     };
 
-
     function formatDate(date) {
-
-        // Create a Date object from the input
         const dateObj = new Date(date);
-
-        // Get the day of the month
         const day = dateObj.getDate();
         const year = dateObj.getFullYear();
-
-        // Get the month index (0-11) and map it to month name
         let month = dateObj.getMonth();
         const suffix = day === 1 || day === 21 || day === 31 ? 'st' : (day === 2 || day === 22 ? 'nd' : (day === 3 || day === 23 ? 'rd' : 'th'));
         const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         month = monthNames[month];
         return `${day}${suffix} ${month}'${year}`;
     }
-// console.log(planetEvent[0]);
+
     return (
         <div className="mb-8">
             <h2 className="text-xl font-semibold mb-4 text-center">Planets {selectedPlanet?.name} through the zodiac</h2>
-            <div className="bg-[#3D3E4F] p-4 rounded-xl">
-                {planetEvent && planetEvent.length > 0  ? planetEvent?.map((planet, index) => (
-                    <div key={index}>
-                        <div className="text-center mb-4 ">
+            <div className="bg-[#3D3E4F] p-4 rounded-xl max-h-80 overflow-y-auto">
+                {planetEvent && planetEvent.length > 0 ? (
+                    planetEvent.map((planet, index) => (
+                        <div key={index} className="mb-4">
                             <div
                                 className="space-x-2 mb-2 mt-3 flex items-between justify-between cursor-pointer"
                                 onClick={() => handlePlanetSubEvent(index)}
@@ -51,12 +44,11 @@ export const PlanetaryEventAccordion = ({ planetEvent }) => {
                                 </p>
                             </div>
                         </div>
-                    </div>
-                )) : (
+                    ))
+                ) : (
                     <Loader />
                 )}
             </div>
         </div>
-
-    )
-}
+    );
+};
--- pages/planets.js
+++ pages/planets.js
@@ -105,7 +105,7 @@ const LifeArenaScreen = () => {
 
 
 
-                {/* <PlanetaryHours /> */}
+                <PlanetaryHours />
 
                 <CandlestickChart />
 
