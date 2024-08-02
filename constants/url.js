4c81fb8ac731d6c1f8bf5663681fa76b5f741e3c
Nischal Gupta <nischal@onecorp.co.in>
2024-08-02 18:27:27 +0530

    integrated local_transit pana

--- constants/url.js
+++ constants/url.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const astroServer = axios.create({
-  // baseURL: "https://astroserver.onrender.com",
+  baseURL: "https://astroserver.onrender.com",
   // baseURL: "http://192.168.29.194:8000",
-  baseURL: "http://192.168.1.33:7000",
+  // baseURL: "http://192.168.1.33:7000",
 });
 
 
